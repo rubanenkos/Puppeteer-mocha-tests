745ea88699f31377e90eb771d73d50f974522a27
Sergey Rubanenko <srubanenko@aligntech.com>
2022-04-24 12:35:21 +0300

    Config tests

---
+++ build/tests/example.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('First test', function () {
+    it('It should work', function () {
+        console.log('Yes');
+    });
+});
---
+++ src/tests/example.test.js
@@ -0,0 +1,5 @@
+describe('First test', () =>{
+    it('It should work', () => {
+        console.log('Yes');
+    });
+});
